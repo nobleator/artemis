774a12a73263bd90985753bbf2e3845f808f4103
nobleator <noble.ben.16@gmail.com>
2025-12-09 09:24:04 -0500

    Fix broken SQL

--- avalonia/Artemis.Infra/Repositories/PointOfInterestRepository.cs
+++ avalonia/Artemis.Infra/Repositories/PointOfInterestRepository.cs
@@ -29,9 +29,11 @@ public class PointOfInterestRepository(IDbConnection db) : IPointOfInterestRepos
     public async Task BulkInsertAsync(IEnumerable<PointOfInterest> poiList, CancellationToken ct = default)
     {
         var insert = @"
-            insert into poi (category_id, lat, lon)
-            values (@Category, @Latitude, @Longitude)
-            where not exists (select 1 from poi where source_xref = @SourceXref);";
+            insert into poi (source_xref, category_id, lat, lon)
+            select @SourceXref, @Category, @Latitude, @Longitude
+            where not exists (
+                select 1 from poi where source_xref = @SourceXref
+            );";
         using var conn = _db;
         conn.Open();
         using var tx = _db.BeginTransaction();
