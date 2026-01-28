810a6d75b21dc1a35984fe88ec58751ad69687f7
nobleator <noble.ben.16@gmail.com>
2026-01-28 13:18:17 -0500

    Convert csv path to relative reference

--- src/Data.fs
+++ src/Data.fs
@@ -75,7 +75,7 @@ module Locations =
                         )
                     ) AS p
                 FROM read_csv(
-                    '~/Projects/artemis/data/listings.csv',
+                    './data/listings.csv',
                     delim=',',
                     header=false,
                     columns={
